de395a6b4f411bae515f4fd07613b3c30c31b9e3
Bob Witmer <bwitmer@golden.net>
2023-08-25 15:26:19 -0400

    Initial Commit

---
+++ PlaceLookupDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PlaceLookupDemo
+//
+//  Created by Bob Witmer on 2023-08-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Bob Witmer on 2023-08-25.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
